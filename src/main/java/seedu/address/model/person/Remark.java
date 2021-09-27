15e5017c9b087e25013bd65a95d85cf1d1c900a2
jiarong15 <jiarong15@gmail.com>
2021-09-27 19:51:37 +0800

    Rectify import orders

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,11 +9,19 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Creates a remark object with a string content.
+     * @param remark String of content.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
     }
 
+    /**
+     * Gets the string contents of a remark object.
+     * @return String of the remark contents.
+     */
     @Override
     public String toString() {
         return value;
@@ -26,6 +34,10 @@ public class Remark {
                 && value.equals(((Remark) other).value)); // state check
     }
 
+    /**
+     * Gets content of remark object hashcode.
+     * @return Integer hashcode value
+     */
     @Override
     public int hashCode() {
         return value.hashCode();
